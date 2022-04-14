79aa6f658b80e22c1fe29d1ee349477c1c7b3021
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-04-13 22:48:02 -0300

    feat: add filter category

--- js/products.js
+++ js/products.js
@@ -1,6 +1,11 @@
 const head_div = document.querySelector(".products-head");
 const body_div = document.querySelector(".products-body");
 
+const category_div = document.querySelector(".category");
+const search_input = document.getElementById("search_input");
+const filter_btn = document.querySelector(".filter");
+const clean_btn = document.querySelector(".clean");
+
 const renderHead = async () => {
     const url = "http://localhost:3000/products";
     const products = await fetch(url).then((response) => response.json());
@@ -17,8 +22,15 @@ const redireciona = (id) => {
     window.location.replace(`/editProduct.html?id=${id}`);
 }
 
-const renderBody = async () => {
-    const url = "http://localhost:3000/products";
+const renderBody = async (filter) => {
+    let url = "";
+
+    if(filter){
+        url = `http://localhost:3000/products?${filter.category}=${filter.description}`;
+    } else{
+        url = `http://localhost:3000/products`;
+    }
+   
     const products = await fetch(url).then((response) => response.json());
     
     let template = "";
@@ -49,5 +61,26 @@ const renderBody = async () => {
    
     body_div.innerHTML = template;
 }
+
+filter_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const filter = {
+        category: category_div.options[category_div.selectedIndex].value,
+        description: search_input.value
+    }
+
+    renderBody(filter);
+});
+
+clean_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    category_div.value = "default"; 
+    search_input.value = "";
+    renderBody();
+});
+
+
 
 window.addEventListener("DOMContentLoaded", () => renderHead(), renderBody());
